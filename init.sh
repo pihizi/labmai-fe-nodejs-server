14453839647308ceadeada0810aa770b2ed625c0
洪小兵 <pihizi@msn.com>
2018-04-13 06:29:57 +0000

    允许配置源

--- init.sh
+++ init.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-cd /usr/src/app && npm install
+NPM_SOURCE=$NPM_SOURCE_URL
+if [ "-${NPM_SOURCE}-" = "--" ] 
+then
+    cd /usr/src/app && npm install
+else 
+    cd /usr/src/app && npm --registry=$NPM_SOURCE install
+fi
